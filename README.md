cbe2e458c9cb4a245cb9f78c68708b5427f2db49
Mike Davis <mike@michaeljacobdavis.com>
2014-06-11 21:50:11 -0700

    Added build status

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Angular App
+Angular App [![Build Status](https://travis-ci.org/yata-yata/angular-modules.svg?branch=master)](https://travis-ci.org/yata-yata/angular-modules)``
 ===========
 
 Not standalone. Used in UI and Mobile.
---
+++ test/index.js
@@ -0,0 +1,22 @@
+// Load modules
+
+var Lab = require('lab'),
+    modules = require('../'),
+
+    // Declare internals
+    internals = {},
+
+    // Test aliases
+    expect = Lab.expect,
+    before = Lab.before,
+    beforeEach = Lab.beforeEach,
+    after = Lab.after,
+    afterEach = Lab.afterEach,
+    describe = Lab.experiment,
+    it = Lab.test,
+    assert = Lab.assert;
+
+describe('angular-modules', function () {
+});
+
+
